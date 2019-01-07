9e74e8bfa08e8826688aa67d15b69294b50ac678
mihail-yunchov <misho_yunchov@yahoo.com>
2019-01-08 01:06:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -11,73 +11,77 @@ var collectionFaces = 'collectionFaces';
 
 var rekognition = new AWS.Rekognition();
 rekognition.listCollections({}, function (err, data) {
-    if (data.CollectionIds.indexOf(collectionFaces) === -1) {
-        rekognition.createCollection({ CollectionId: collectionFaces }, function (err, data) {
-            console.log(err, data);
-        })
-    }
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  if (data.CollectionIds.indexOf(collectionFaces) === -1) {
+    rekognition.createCollection({CollectionId: collectionFaces}, function (err, data) {
+      console.log(err, data);
+    })
+  }
 })
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/admin', function (req, res) {
-    res.sendFile(path.join(__dirname + '/admin.html'));
+  res.sendFile(path.join(__dirname + '/admin.html'));
 });
 
 app.post('/upload-image', multiMiddleware, function (req, res) {
-    var file = req.files.image;
+  var file = req.files.image;
 
-    fs.readFile(file.path, function (err, data) {
-        var params = {
-            CollectionId: collectionFaces,
-            DetectionAttributes: [
-            ],
-            ExternalImageId: file.originalFilename,
-            Image: {
-                Bytes: data
-            }
-        };
-        rekognition.indexFaces(params, function (err, data) {
-            if (err) {
-                console.log('ERROR MSG: ', err);
-                res.status(500).send(err);
-            } else {
-                console.log('Successfully uploaded data');
-                res.status(200).end();
-            }
-        });
+  fs.readFile(file.path, function (err, data) {
+    var params = {
+      CollectionId: collectionFaces,
+      DetectionAttributes: [],
+      ExternalImageId: file.originalFilename,
+      Image: {
+        Bytes: data
+      }
+    };
+    rekognition.indexFaces(params, function (err, data) {
+      if (err) {
+        console.log('ERROR MSG: ', err);
+        res.status(500).send(err);
+      } else {
+        console.log('Successfully uploaded data');
+        res.status(200).end();
+      }
     });
+  });
 })
 
 app.post('/recognize', multiMiddleware, function (req, res) {
-    var rekognition = new AWS.Rekognition();
-    var file = req.files.image;
-    fs.readFile(file.path, function (err, data) {
-        var params = {
-            CollectionId: collectionFaces,
-            FaceMatchThreshold: 95,
-            Image: {
-                Bytes: data
-            },
-            MaxFaces: 1
-        };
-        rekognition.searchFacesByImage(params, function (err, data) {
-            if (err) {
-                console.log('ERROR MSG: ', err);
-                res.status(400).send('no match');
-            } else {
-                if (data.FaceMatches.length === 0) {
-                    res.status(400).send('no match');
-                } else {
-                    res.status(200).end();
-                }
-               
-            }
-        })
-    });
-})
+  var rekognition = new AWS.Rekognition();
+  var file = req.files.image;
+  fs.readFile(file.path, function (err, data) {
+    var params = {
+      CollectionId: collectionFaces,
+      FaceMatchThreshold: 95,
+      Image: {
+        Bytes: data
+      },
+      MaxFaces: 1
+    };
+    rekognition.searchFacesByImage(params, function (err, data) {
+      if (err) {
+        console.log('ERROR MSG: ', err);
+        res.status(400).send('no match');
+      } else {
+        if (data.FaceMatches.length === 0) {
+          res.status(400).send('no match');
+        } else {
+          res.status(200).end();
+        }
 
-console.log('start listeing on 8080');
-app.listen(process.env.port || 8080);
+      }
+    })
+  });
+})
+var port = process.env.PORT || 8080;
+console.log('start listeing on' + port);
+app.listen(port);
